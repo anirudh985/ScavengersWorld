4023e37758774ba1ecdd2f5a74388e95df104995
01001010n <barry.159@osu.edu>
2016-10-17 16:11:56 -0400

    Proper utilization of new string resources, fixed BaseActivity issues

--- app/src/main/java/com/example/aj/scavengersworld/Activities/LeaderboardActivity.java
+++ app/src/main/java/com/example/aj/scavengersworld/Activities/LeaderboardActivity.java
@@ -1,8 +1,6 @@
 package com.example.aj.scavengersworld.Activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.example.aj.scavengersworld.R;
@@ -10,64 +8,57 @@ import com.example.aj.scavengersworld.R;
 /**
  * Created by Jennifer on 10/17/2016.
  */
-public class LeaderboardActivity extends AppCompatActivity {
-	Toolbar toolbar;
+public class LeaderboardActivity extends BaseActivity {
 
 	private final String LOG_TAG = getClass().getSimpleName();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.d(LOG_TAG, "onCreate()");
-		setContentView(R.layout.activity_leaderboard);
-		configureToolbar();
+		Log.d(LOG_TAG, getString(R.string.log_onCreate));
 	}
 
-	//protected abstract String getScreenName(); TODO??
+	@Override
+	public int getLayoutResource(){
+		return R.layout.activity_notifications;
+	}
 
-	private void configureToolbar() {
-		Log.d(LOG_TAG, "configureToolbar()");
-		toolbar = (Toolbar) findViewById(R.id.toolbar);
-		if (toolbar != null) {
-			setSupportActionBar(toolbar);
-			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-			//getSupportActionBar().setTitle(getScreenName()); TODO??
-
-		}
+	public String getScreenName(){
+		return "Profile";
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
-		Log.d(LOG_TAG, "******* onStart() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onStart));
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		Log.d(LOG_TAG, "******* onResume() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onResume));
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		Log.d(LOG_TAG, "******* onPause() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onPause));
 	}
 
 	@Override
 	protected void onStop() {
 		super.onStop();
-		Log.d(LOG_TAG, "******* onStop() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onStop));
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		Log.d(LOG_TAG, "******* onDestroy() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onDestroy));
 	}
 
 	@Override
 	protected void onRestart() {
 		super.onRestart();
-		Log.d(LOG_TAG, "******* onRestart() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onRestart));
 	}
 }
