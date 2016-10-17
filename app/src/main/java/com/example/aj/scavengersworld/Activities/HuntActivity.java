5fb63df3cf549f60ba8dc5d887a9e35c6f0461d8
01001010n <barry.159@osu.edu>
2016-10-17 16:11:07 -0400

    Fixed onClick, utilized new string resources, fixed issues wrt BaseActivity

--- app/src/main/java/com/example/aj/scavengersworld/Activities/HuntActivity.java
+++ app/src/main/java/com/example/aj/scavengersworld/Activities/HuntActivity.java
@@ -2,7 +2,6 @@ package com.example.aj.scavengersworld.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,14 +13,12 @@ import com.example.aj.scavengersworld.R;
  * Created by Jennifer on 10/17/2016.
  */
 public class HuntActivity extends BaseActivity implements View.OnClickListener {
-	Toolbar toolbar;
 
 	private final String LOG_TAG = getClass().getSimpleName();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.d(LOG_TAG, "onCreate()");
-		setContentView(R.layout.activity_hunt);
+		Log.d(LOG_TAG, getString(R.string.log_onCreate));
 
 		TextView hunt = (TextView) findViewById(R.id.hunt_name);
 		hunt.setText("Hunt Name Here"); //TODO
@@ -29,8 +26,8 @@ public class HuntActivity extends BaseActivity implements View.OnClickListener {
 		TextView description = (TextView) findViewById(R.id.hunt_description);
 		description.setText("Hunt Description Here"); //TODO
 
-		Button join = (Button) findViewById(R.id.hunt_join_button);
-		join.setOnClickListener(this);
+		//Button join = (Button) findViewById(R.id.hunt_join_button);
+		//join.setOnClickListener(this); TODO
 
 		Button leaders = (Button) findViewById(R.id.hunt_leaders_button);
 		leaders.setOnClickListener(this);
@@ -48,37 +45,37 @@ public class HuntActivity extends BaseActivity implements View.OnClickListener {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		Log.d(LOG_TAG, "******* onStart() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onStart));
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		Log.d(LOG_TAG, "******* onResume() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onResume));
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		Log.d(LOG_TAG, "******* onPause() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onPause));
 	}
 
 	@Override
 	protected void onStop() {
 		super.onStop();
-		Log.d(LOG_TAG, "******* onStop() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onStop));
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		Log.d(LOG_TAG, "******* onDestroy() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onDestroy));
 	}
 
 	@Override
 	protected void onRestart() {
 		super.onRestart();
-		Log.d(LOG_TAG, "******* onRestart() **********");
+		Log.d(LOG_TAG, getString(R.string.log_onRestart));
 	}
 
 	@Override
@@ -86,6 +83,7 @@ public class HuntActivity extends BaseActivity implements View.OnClickListener {
 		switch (v.getId()){
 			case R.id.hunt_leaders_button:
 				Intent leaderboard = new Intent(HuntActivity.this,LeaderboardActivity.class);
+				startActivity(leaderboard);
 				break;
 			default:
 				break;
