420dd8cedcd1c0b432139c6f6072cc79a1eedbc0
kavskalyan <kalyan@Kalyans-MacBook-Pro.local>
2016-10-16 20:52:54 -0400

    Implemented both the map activities
    1.GamePlayActivity
    2.ModifyClueLocationActivity

--- app/src/main/java/com/example/aj/scavengersworld/MainActivity.java
+++ app/src/main/java/com/example/aj/scavengersworld/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.aj.scavengersworld;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.aj.scavengersworld.ModifyClueLocationActivity;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button mButton;
+    private Button mGamePlayButton;
+    private TextView mTextView;
+    private static double DEFAULT_LATITUDE = 23.5;
+    private static double DEFAULT_LONGITUDE = 83;
+    private double mCurrentLatitude;
+    private double mCurrentLongitude;
+
+    public MainActivity() {
+        mCurrentLatitude = DEFAULT_LATITUDE;
+        mCurrentLongitude = DEFAULT_LONGITUDE;
+    }
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button)findViewById(R.id.button2);
+        mGamePlayButton = (Button)findViewById(R.id.button3);
+        mButton.setOnClickListener(this);
+        mGamePlayButton.setOnClickListener(this);
+        mTextView = (TextView)findViewById(R.id.textView2);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button2){
+            Intent editLocationIntent = new Intent(MainActivity.this,ModifyClueLocationActivity.class);
+            //Intent editLocationIntent = new Intent(MainActivity.this,GamePlayActivity.class);
+            //Bundle initLocation = new Bundle();
+            editLocationIntent.putExtra("LATITUDE",mCurrentLatitude);
+            editLocationIntent.putExtra("LONGITUDE",mCurrentLongitude);
+            startActivityForResult(editLocationIntent,2);
+        }
+        else if(v.getId() == R.id.button3){
+            //Intent editLocationIntent = new Intent(MainActivity.this,ModifyClueLocationActivity.class);
+            Intent editLocationIntent = new Intent(MainActivity.this,GamePlayActivity.class);
+            //Bundle initLocation = new Bundle();
+            editLocationIntent.putExtra("LATITUDE",mCurrentLatitude);
+            editLocationIntent.putExtra("LONGITUDE",mCurrentLongitude);
+            startActivityForResult(editLocationIntent,2);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if(requestCode==2)
+        {
+            double lLatitude=data.getDoubleExtra("LATITUDE",-1);
+            double lLongitude=data.getDoubleExtra("LONGITUDE",-1);
+            String message = Double.toString(lLatitude) +","+ Double.toString(lLongitude);
+            mTextView.setText(message);
+        }
     }
 }
